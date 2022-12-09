58293ddcdb4d1ab462ead7b323e5b5d970e20d36
Jonathan <32264711+JonaVDM@users.noreply.github.com>
2022-12-09 11:10:09 +0100

    Day 9 part 2

--- day09/day09.go
+++ day09/day09.go
@@ -10,11 +10,14 @@ import (
 func Run(file string) [2]interface{} {
 	data := utils.ReadFile(file)
 
-	headX, headY := 0, 0
-	prevHeadX, prevHeadY := 0, 0
-	tailX, tailY := 0, 0
-	been := make(map[string]bool)
-	been["0,0"] = true
+	ropeOne := [2][2]int{}
+	ropeTwo := [10][2]int{}
+
+	beenOne := make(map[string]bool)
+	beenOne["0,0"] = true
+
+	beenTwo := make(map[string]bool)
+	beenTwo["0,0"] = true
 
 	for _, row := range data {
 		instruction := row[0]
@@ -24,33 +27,69 @@ func Run(file string) [2]interface{} {
 		}
 
 		for i := 0; i < amount; i++ {
-			prevHeadX = headX
-			prevHeadY = headY
-
 			if instruction == 'U' {
-				headY += 1
+				ropeOne[0][0] -= 1
+				ropeTwo[0][0] -= 1
 			} else if instruction == 'D' {
-				headY -= 1
+				ropeOne[0][0] += 1
+				ropeTwo[0][0] += 1
 			} else if instruction == 'L' {
-				headX -= 1
+				ropeOne[0][1] -= 1
+				ropeTwo[0][1] -= 1
 			} else if instruction == 'R' {
-				headX += 1
+				ropeOne[0][1] += 1
+				ropeTwo[0][1] += 1
 			} else {
 				panic("Invalid instruction")
 			}
 
 			// part one
-			if utils.AbsInt(tailX-headX) == 2 || utils.AbsInt(tailY-headY) == 2 {
-				tailX = prevHeadX
-				tailY = prevHeadY
-
-				been[fmt.Sprintf("%d,%d", tailX, tailY)] = true
+			if calculateDistance(ropeOne[0], ropeOne[1]) == 2 {
+				ropeOne[1] = moveTowards(ropeOne[0], ropeOne[1])
+				beenOne[fmt.Sprintf("%d,%d", ropeOne[1][0], ropeOne[1][1])] = true
 			}
+
+			// part two
+			for i := 1; i <= 9; i++ {
+				if calculateDistance(ropeTwo[i-1], ropeTwo[i]) == 2 {
+					ropeTwo[i] = moveTowards(ropeTwo[i-1], ropeTwo[i])
+				}
+			}
+			beenTwo[fmt.Sprintf("%d,%d", ropeTwo[9][0], ropeTwo[9][1])] = true
 		}
 	}
 
 	return [2]interface{}{
-		len(been),
-		0,
+		len(beenOne),
+		len(beenTwo),
 	}
+}
+
+func calculateDistance(head, tail [2]int) int {
+	// https://en.wikipedia.org/wiki/Chebyshev_distance
+	return utils.Max([]int{utils.AbsInt(head[0] - tail[0]), utils.AbsInt(head[1] - tail[1])})
+}
+
+func moveTowards(head, tail [2]int) [2]int {
+	// up
+	if head[1] > tail[1] {
+		tail[1] += 1
+	}
+
+	// down
+	if head[1] < tail[1] {
+		tail[1] -= 1
+	}
+
+	// left
+	if head[0] < tail[0] {
+		tail[0] -= 1
+	}
+
+	// right
+	if head[0] > tail[0] {
+		tail[0] += 1
+	}
+
+	return tail
 }
