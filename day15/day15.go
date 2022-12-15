fd50b7321adb8fbdf086fab2ce39d91ac32f606b
Jonathan <32264711+JonaVDM@users.noreply.github.com>
2022-12-15 18:02:10 +0000

    Just a bit faster

--- day15/day15.go
+++ day15/day15.go
@@ -1,6 +1,7 @@
 package day15
 
 import (
+	"fmt"
 	"strconv"
 	"strings"
 
@@ -9,51 +10,12 @@ import (
 
 func Run(file string) [2]interface{} {
 	data := utils.ReadFile(file)
-	targetY := 2000000
-
-	sensorTarget := make(map[int]bool)
-	beaconList := make(map[int]bool)
 
 	rows := parse(data)
 
-	// part one
-	for _, row := range rows {
-		distance := utils.AbsInt(row.SensorX-row.BeaconX) + utils.AbsInt(row.SensorY-row.BeaconY)
-		difference := utils.AbsInt(targetY - row.SensorY)
-		if difference > distance {
-			continue
-		}
-
-		sensorTarget[row.SensorX] = true
-
-		for i := 1; i < distance-difference+1; i++ {
-			sensorTarget[row.SensorX+i] = true
-			sensorTarget[row.SensorX-i] = true
-		}
-
-		if row.BeaconY == targetY {
-			beaconList[row.BeaconX] = true
-		}
-	}
-
-	ret := make(chan int)
-	for i := 0; i < 4000000; i += 100000 {
-		go findPartTwo(i, i+100000-1, rows, ret)
-	}
-
-	b := 0
-	for {
-		res := <-ret
-		if res == -1 {
-			continue
-		}
-		b = res
-		break
-	}
-
 	return [2]interface{}{
-		len(sensorTarget) - len(beaconList),
-		b,
+		partOne(rows),
+		partTwo(rows),
 	}
 }
 
@@ -63,6 +25,8 @@ type Row struct {
 
 	BeaconX int
 	BeaconY int
+
+	Manhatten int
 }
 
 func parse(data []string) []Row {
@@ -81,42 +45,115 @@ func parse(data []string) []Row {
 			sensorY,
 			beaconX,
 			beaconY,
+			utils.AbsInt(sensorX-beaconX) + utils.AbsInt(sensorY-beaconY),
 		})
 	}
 
 	return rows
 }
 
-func findPartTwo(start, end int, rows []Row, ret chan int) {
-	for i := start; i < end; i++ {
-		line := make([]bool, 4000000)
+func partOne(rows []Row) int {
+	targetY := 2000000
+	sensorTarget := make(map[int]bool)
+	beaconList := make(map[int]bool)
 
-		for _, row := range rows {
-			distance := utils.AbsInt(row.SensorX-row.BeaconX) + utils.AbsInt(row.SensorY-row.BeaconY)
-			difference := utils.AbsInt(i - row.SensorY)
-			if difference > distance {
+	for _, row := range rows {
+		difference := utils.AbsInt(targetY - row.SensorY)
+		if difference > row.Manhatten {
+			continue
+		}
+
+		sensorTarget[row.SensorX] = true
+
+		for i := 1; i < row.Manhatten-difference+1; i++ {
+			sensorTarget[row.SensorX+i] = true
+			sensorTarget[row.SensorX-i] = true
+		}
+
+		if row.BeaconY == targetY {
+			beaconList[row.BeaconX] = true
+		}
+	}
+
+	return len(sensorTarget) - len(beaconList)
+}
+
+func partTwo(rows []Row) int {
+	matching := make(map[string]int)
+
+	for i := 0; i < len(rows); i++ {
+		for x := i + 1; x < len(rows); x++ {
+			dis := utils.AbsInt(rows[i].SensorX-rows[x].SensorX) + utils.AbsInt(rows[i].SensorY-rows[x].SensorY)
+			if dis != rows[i].Manhatten+rows[x].Manhatten+2 {
 				continue
 			}
 
-			line[row.SensorX] = true
+			var big Row
+			var small Row
 
-			for i := 1; i < distance-difference+1; i++ {
-				if row.SensorX-i >= 0 {
-					line[row.SensorX-i] = true
-				}
-
-				if row.SensorX+i < 4000000 {
-					line[row.SensorX+i] = true
-				}
+			if rows[i].Manhatten < rows[x].Manhatten {
+				// i is smaller
+				big = rows[x]
+				small = rows[i]
+			} else {
+				// x is smaller
+				big = rows[i]
+				small = rows[x]
 			}
-		}
 
-		for x, l := range line {
-			if !l {
-				ret <- 4000000*x + i
+			x := small.Manhatten + 1
+			y := 0
+
+			for {
+				var key string = "woeps"
+				// Top left
+				if small.SensorX < big.SensorX && small.SensorY < big.SensorY {
+					key = fmt.Sprintf("%d,%d", small.SensorX+x, small.SensorY+y)
+				}
+
+				// Top right
+				if small.SensorX > big.SensorX && small.SensorY < big.SensorY {
+					key = fmt.Sprintf("%d,%d", small.SensorX-x, small.SensorY+y)
+				}
+
+				// Bottom left
+				if small.SensorX < big.SensorX && small.SensorY > big.SensorY {
+					key = fmt.Sprintf("%d,%d", small.SensorX+x, small.SensorY-y)
+				}
+
+				// Bottom right
+				if small.SensorX > big.SensorX && small.SensorY > big.SensorY {
+					key = fmt.Sprintf("%d,%d", small.SensorX-x, small.SensorY-y)
+				}
+
+				if key == "woeps" {
+					panic("woeps")
+				}
+
+				matching[key]++
+
+				if x == 0 {
+					break
+				}
+
+				y++
+				x--
 			}
 		}
 	}
 
-	ret <- -1
+	max := 0
+	maxKey := ""
+	for k, value := range matching {
+		if value > max {
+			max = value
+			maxKey = k
+		}
+	}
+
+	key := strings.Split(maxKey, ",")
+	x, _ := strconv.Atoi(key[0])
+	y, _ := strconv.Atoi(key[1])
+
+	return 4000000*x + y
 }
